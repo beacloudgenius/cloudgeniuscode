452d3fd318565c60070484ed228cbcb45c803d2e
Nilesh Londhe <nilesh@cloudgeni.us>
2019-12-15 20:29:57 -0800

    edits s

--- installation.ps1
+++ installation.ps1
@@ -7,6 +7,17 @@ iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco feature enable -n allowGlobalConfirmation
 choco install vscode -Force
 setx path "%path%;'C:\Program Files\Microsoft VS Code\bin'"
+$env:Path += ";'C:\Program Files\Microsoft VS Code\bin'"
+#VSCODE EXTENSIONS
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension ms-vscode-remote.remote-ssh
+if (Test-Path "~/AppData/Roaming/Code/User") {pwd} else {mkdir "~/AppData/Roaming/Code/User"}
+wget -o ~/AppData/Roaming/Code/User/settings.json https://s3-us-west-2.amazonaws.com/cloudgeniuscode/settings.json
+
 choco install winscp -Force
+choco install git -Force
+choco install miniconda3 -Force
 Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+Remove-Item C:\Users\*\Desktop\"Visual Studio Code".lnk -Force
+Remove-Item C:\Users\*\Desktop\WinSCP.lnk -Force
--- keypair-sg.ps1
+++ keypair-sg.ps1
@@ -8,7 +8,7 @@ $CloudGenius | Get-Member
 $CloudGenius | Format-List KeyName, KeyFingerprint, KeyMaterial
 Get-EC2KeyPair -KeyName CloudGenius | format-list KeyName, KeyFingerprint
 $CloudGenius.KeyMaterial | Out-File -Encoding ascii CloudGenius.pem
-if (Test-Path "~/.ssh") {pwd} else {mkdir ./.ssh}
+if (Test-Path "~/.ssh") {pwd} else {mkdir ~/.ssh}
 $FileName = "~/.ssh/CloudGenius.pem"
 if (Test-Path $FileName) {
   Remove-Item $FileName
@@ -39,7 +39,7 @@ New-EC2Tag -ResourceId $groupid  -Tags $Tags
 
 #VSCODE EXTENSIONS
 code --install-extension ms-vscode-remote.remote-ssh
-wget -o ~/AppData/Roaming/Code/User/settings.json https://gist.githubusercontent.com/lvnilesh/523afc0e19f4dba3bca6e6c2fc99bbaa/raw/2ee1c531083ba771f8fd9d3c74b8415198038954/settings.json
+wget -o ~/AppData/Roaming/Code/User/settings.json https://s3-us-west-2.amazonaws.com/cloudgeniuscode/settings.json
 
 #PROVISIONER
-wget -o provision.txt https://gist.githubusercontent.com/lvnilesh/a96eb78b9f49bbf779ef60ace04f2405/raw/7c5033086628f2270364f5674f62a906f4bec5f3/provision.txt
+wget -o provision.txt https://s3-us-west-2.amazonaws.com/cloudgeniuscode/provision.txt
