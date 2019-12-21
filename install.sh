731aa55ccc7d8976d6456bd15589c156e7332c89
Nilesh Londhe <nilesh@cloudgeni.us>
2019-12-20 16:40:19 -0800

    updates

--- install.sh
+++ install.sh
@@ -5,3 +5,13 @@ code --install-extension ms-vscode-remote.remote-ssh
 rm -rf settings.json
 curl -O https://s3-us-west-2.amazonaws.com/cloudgeniuscode/settings.json
 mv -f settings.json "$HOME/Library/Application Support/Code/User/"
+
+rm -rf $HOME/miniconda ~/miniconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+rm -rf ~/miniconda.sh
+source $HOME/miniconda/bin/activate
+conda init zsh
+conda config --set auto_activate_base false
+conda update -n base -c defaults conda -y
+conda update --all -y
