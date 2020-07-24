83f56867d4a12581852259237405165c859165cf
Nilesh Londhe <nilesh@cloudgeni.us>
2020-07-23 21:12:38 -0700

    base64 eate ssh config sd to 30 to retain free tier benefits

--- keypair-and-sg.sh
+++ keypair-and-sg.sh
@@ -1,8 +1,8 @@
 yes y | ssh-keygen -t rsa -b 4096 -N "" -C "CloudGenius" -f ~/.ssh/CloudGenius > /dev/null
 aws ec2 delete-key-pair --key-name Key-only-for-use-with-CloudGenius-workstation
-rm -rf ~/.ssh/CloudGenius.pub.base64
-cat ~/.ssh/CloudGenius.pub|base64 >  ~/.ssh/CloudGenius.pub.base64
-aws ec2 import-key-pair --key-name "Key-only-for-use-with-CloudGenius-workstation" --public-key-material file://~/.ssh/CloudGenius.pub.base64
+# rm -rf ~/.ssh/CloudGenius.pub.base64
+# cat ~/.ssh/CloudGenius.pub|base64 >  ~/.ssh/CloudGenius.pub.base64
+aws ec2 import-key-pair --key-name "Key-only-for-use-with-CloudGenius-workstation" --public-key-material file://~/.ssh/CloudGenius.pub
 aws ec2 delete-security-group --group-name SG-only-for-use-with-CloudGenius-workstation > /dev/null 2>&1
 security_response=$(aws ec2 create-security-group \
  --group-name "SG-only-for-use-with-CloudGenius-workstation" \
