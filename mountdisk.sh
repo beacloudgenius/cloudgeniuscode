538486b3aaa2d06be31d28995c01495534191545
Nilesh <nilesh@cloudgeni.us>
2021-08-02 19:40:09 -0700

    edits

--- mountdisk.sh
+++ mountdisk.sh
@@ -1,13 +1,13 @@
 # df -H
 # lsblk
-# sudo file -s /dev/xvdf
-sudo mkfs -t xfs /dev/xvdf
-sudo file -s /dev/xvdf
+# sudo file -s /dev/nvme1n1
+sudo mkfs -t xfs /dev/nvme1n1
+sudo file -s /dev/nvme1n1
 sudo mkdir -p /cloudgenius
-sudo mount /dev/xvdf /cloudgenius
+sudo mount /dev/nvme1n1 /cloudgenius
 sudo cp /etc/fstab /etc/fstab.orig
 sudo tee -a /etc/fstab > /dev/null << EOF
-UUID=$(lsblk -o +UUID /dev/xvdf -o UUID |grep -v UUID)  /cloudgenius  xfs  defaults,nofail  0  2
+UUID=$(lsblk -o +UUID /dev/nvme1n1 -o UUID |grep -v UUID)  /cloudgenius  xfs  defaults,nofail  0  2
 EOF
 sudo umount /cloudgenius
 sudo mount -a
---
+++ ruby.sh
@@ -0,0 +1,10 @@
+sudo apt install -y curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+sudo apt-get install -y git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+
+rbenv install 2.7.4
+rbenv global 2.7.4
